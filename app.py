b4a6dc04cb80e83d27bdb50f8ff91ba297a09bf0
dynapark-yj <giannebaba@gmail.com>
2025-05-25 15:42:36 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,38 +1,40 @@
-
-
 import streamlit as st
-from stock_info import stock
-from report_service import investment_report
+
+from stock_info import Stock
+from report_service import invesment_report
 from search import stock_search
 
 class SearchResult:
-    def _init_(self,item:
-               self.item = investment_report)
+    def __init__(self, item):
+        self.item = item
+
     @property
     def symbol(self):
         return self.item["Symbol"]
     
     @property
     def name(self):
-        return f"{self.symbol} {self.name}"
+        return self.item["Name"]
+    
+    def __str__(self):
+        return f"{self.symbol}: {self.name}"
 
+st.title("AI 투자 보고서 생성 서비스")
 
-st.title("AI 추자 보고서 생성 서비스" )
-
-search_query =st.text_input("회사명", "Apple Inc.")
-hits = stck_search(search_query)['hits']
+search_query = st.text_input("회사명", "Apple Inc.")
+hits = stock_search(search_query)['hits']
 search_results = [SearchResult(hit) for hit in hits]
+selected = st.selectbox("검색 결과 리스트", search_results)
+stock = Stock(selected.symbol)
 
-st.selectbox("검색 결과 리스트", search_results )
-
-tabs = ["회사 정보", "AI 투자자 보고서" ]
+tabs = ["회사 정보", "AI 투자 보고서"]
 tab1, tab2 = st.tabs(tabs)
 
+
 with tab1:
-st.header(str(selected))
+    st.header(str(selected))
     st.write(stock.get_basic_info())
     st.write(stock.get_financial_statement())
-
 
 with tab2:
     st.header("AI 투자 보고서")
@@ -41,4 +43,3 @@ with tab2:
             report = invesment_report(selected.name, selected.symbol)
             st.success("Done.")
         st.write(report)
-
--- stock_info.py
+++ stock_info.py
@@ -1,17 +1,16 @@
-
-
 import yfinance as yf
-import pandas as pd 
+import pandas as pd
 
-class stock:
-    def _init_(self, symbol):
+class Stock:
+    def __init__(self, symbol):
         self.symbol = symbol
         self.ticker = yf.Ticker(symbol)
+
     def get_basic_info(self):
-        pd.DataFrame.from_dict(self.ticker.info, orient="index", columns=["Values"])
-        return basic_info.loc[['longName, 'industry', 'sector'', 'marketCap', 'shareOutstanding']].to_markdown()
-    def getfinancial_statement(self):
-        def get_financial_statement(self):
+        basic_info = pd.DataFrame.from_dict(self.ticker.info, orient="index", columns=["Values"])
+        return basic_info.loc[['longName', 'industry', 'sector', 'marketCap', 'sharesOutstanding']].to_markdown()
+    
+    def get_financial_statement(self):
         return f"""
         ### Quarterly Income Statement
         {self.ticker.quarterly_income_stmt.loc[['Total Revenue', 'Gross Profit', 'Operating Income', 'Net Income']].to_markdown()}
@@ -20,4 +19,4 @@ class stock:
         {self.ticker.quarterly_balance_sheet.loc[['Total Assets', 'Total Liabilities Net Minority Interest', 'Stockholders Equity']].to_markdown()}
 
         ### Quarterly Cash Flow
-        {self.ticker.quarterly_cash_flow.loc[['Operating Cash Flow', 'Investing Cash Flow', 'Financing Cash Flow']].to_markdown()}ss
+        {self.ticker.quarterly_cash_flow.loc[['Operating Cash Flow', 'Investing Cash Flow', 'Financing Cash Flow']].to_markdown()}"""
